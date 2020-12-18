827e48d0d1b26d1f96e609abea6e7a96c7b4979c
Gerardo <flores20_03@outlook.com>
2020-12-18 15:09:34 -0600

    Sizes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-//console.log("Cargando antes de la función");
+// console.log("Cargando antes de la función");
 (function() {
     "use strict";
-    //console.log("Cargando dentro de la función pero antes del Listener");
+    // console.log("Cargando dentro de la función pero antes del Listener");
     document.addEventListener('DOMContentLoaded', function() {
-        //console.log("Cargando dentro de la función dentro del Listener");
+        console.log("Cargando dentro de la función dentro del Listener");
         //alert("Pues dije que verías cambios pero no dije que bien chidos xd");
+        console.clear();
+        console.log("Sitio hecho para Ricardo Flores Olvera.");
+        console.log("Desarrollado por Gerardo Flores Ramírez.");
+        console.log("ADVERTENCIA: Los errores que se muestran en consola al pasar el cursor sobre el área del sidebar no son importantes y no afectan la ejecución de la página, son solo detalles que surgieron por una de las funciones implementadas en este mismo script, pero no son relevantes. De igual manera se buscará una mejor manera de obtener el mismo resultado (zoom y encabezado que se añade) de una manera que no lanze alertas.");
+        // alert(window.innerWidth);
         eventListeners();
 
         function eventListeners() {
@@ -39,6 +44,6 @@
         }
         /* Fany me dijo "Te quiero mucho" y fue lo primero que leí en el día y me hizo muy feliz */
     }); // DOM CONTENT LOADED
-    //console.log("Cargando dentro de la función pero después del Listener");
+    // console.log("Cargando dentro de la función pero después del Listener");
 })();
-//console.log("Cargando después de la función");
+// console.log("Cargando después de la función");
