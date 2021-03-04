2fd348f305718415fef6a74e6500e8d82f7b4fea
Gerardo <flores20_03@outlook.com>
2021-03-04 00:52:34 -0600

    Un lightbox sencillo

--- js/main.js
+++ js/main.js
@@ -5,6 +5,26 @@
         console.log("Sitio hecho para Ricardo Flores.");
         console.log("Desarrollado por Gerardo Flores.");
 
+        const lightbox=document.createElement('div');
+        lightbox.id='lightbox';
+        document.body.appendChild(lightbox);
+        const images=document.querySelectorAll('img');
+        images.forEach(image=>{
+            image.addEventListener('click',e=>{
+                lightbox.classList.add('active');
+                const img=document.createElement('img');
+                img.src=image.src;
+                while(lightbox.firstChild){
+                    lightbox.removeChild(lightbox.firstChild);
+                }
+                lightbox.appendChild(img);
+            })
+        });
+        lightbox.addEventListener('click',e=>{
+            if(e.target!==e.currentTarget) return;
+            lightbox.classList.remove('active');
+        })
+
         const redireccionar=document.querySelector('#new');
         if(redireccionar){
             const tamanopantalla = window.innerWidth;
