315e2384b61fac4a87d03b95bf8b331b8cf74e24
Gerardo <flores20_03@outlook.com>
2021-03-05 02:05:27 -0600

    Ya casi

--- README.md
+++ README.md
@@ -3,37 +3,12 @@ https://floresr.netlify.app
 
 Portafolio de diseño
 
-Estático pero ps ahí va creciendo la mota
+Estático pero ps ahí va creciendo la mota.
 
-Update: 2do diseño
+Update: 2do diseño.
 
-EN PROCESO:
--Tener ganas
--
-
-COMPLETADO:
--Hacer una carpeta para guardar ahí los archivos del primer diseño
--Renombrar todos los archivos y rutas
--Convertir imágenes a webp (manualmente, me lleva la v)
--Mejorar index
--Agregar marco y tags
--Hacer el nuevo layout
--Animar flechas
--Agregar íconos en workbook
--Mejorar distribución de secciones
--Scroll enhancement
--Barrita nueva
--About me
--Optimizar rendimiento (lazy loading, prefetch)
--Redimensionar para densidades más grandes (1024px width)
-
-PENDIENTE:
--Glitches de título
--Hacer galería
+05/03/2021 - Pues, ya quedó todo. O al menos por ahora.
 
 12/01/2021 - Hoy fue un buen día porque compré unos audífonos
 
 13/01/2021 - Hoy fue un mejor día porque ya me llegaron mis audífonos
-
-@media only screen and (min-width:1600px){
-}
--- js/main.js
+++ js/main.js
@@ -5,26 +5,6 @@
         console.log("Sitio hecho para Ricardo Flores.");
         console.log("Desarrollado por Gerardo Flores.");
 
-        const lightbox=document.createElement('div');
-        lightbox.id='lightbox';
-        document.body.appendChild(lightbox);
-        const images=document.querySelectorAll('img');
-        images.forEach(image=>{
-            image.addEventListener('click',e=>{
-                lightbox.classList.add('active');
-                const img=document.createElement('img');
-                img.src=image.src;
-                while(lightbox.firstChild){
-                    lightbox.removeChild(lightbox.firstChild);
-                }
-                lightbox.appendChild(img);
-            })
-        });
-        lightbox.addEventListener('click',e=>{
-            if(e.target!==e.currentTarget) return;
-            lightbox.classList.remove('active');
-        })
-
         const redireccionar=document.querySelector('#new');
         if(redireccionar){
             const tamanopantalla = window.innerWidth;
@@ -54,6 +34,112 @@
             document.querySelector('.logo a.close').style.display = "none";
             document.querySelector('.logo a.barras').style.display = "block";
         }
+
+
+        /* LIGHTBOX */
+        /* Una manera sencilla con CSS y JavaScript puros */
+        /* Se hace una constante que va a ser el div a crear para posicionar la imagen */
+        const lightbox=document.createElement('div');
+        lightbox.id='lightbox'; // Se le da un ID
+        document.body.appendChild(lightbox); // Se añade el div que acabamos de crear, pero con CSS hacemos que no se muestre desde un inicio
+        const foreground=document.querySelectorAll('.foregroundZoom'); // Esto es para contar la cantidad elementos foregorundZoom en el documento
+        foreground.forEach(fore=>{ // Ciclamos cada foregroudnZoom gracias a la cantidad que acabamos de sacar, teniendo fore como referencia
+            fore.addEventListener('click',e=>{ // Evento de click en cada foregroundZoom
+                lightbox.classList.add('active'); // Se habilita el lighbox cuando se da click al elemento
+                const img=document.createElement('img'); // Creamos un elemento img para nuestro contenedor lightbox
+                img.src=fore.previousElementSibling.src; // Accedemos al atributo source del elemento hermano sobre el que queremos extraer el valor y dárselo
+                                   // al atributo source de la imagen que queremos poner en el lightbox
+                // imageneshija[1].parentElement.previousElementSibling.src
+                // 
+                if(fore.previousElementSibling.classList.contains('vertical')){ // Este bloque detecta si la imagen tiene clase vertical
+                    img.style.width="30%";
+                    img.style.height="100%";
+                }
+                while(lightbox.firstChild){ // Mientras el lightbox tenga un hijo
+                    lightbox.removeChild(lightbox.firstChild); // Se removerá ese hijo
+                    // En caso de que no se removiera el hijo, cada vez que cerremos el lightbox pero volvamos a abrir otra imagen,
+                    // se verán las dos, lo cual no queremos.
+                }
+                lightbox.appendChild(img); // Añadimos ese elemento como hijo al lightbox
+            })
+        });
+
+        const imagenDisenoViejo=document.querySelectorAll('.vieja');
+        imagenDisenoViejo.forEach(imagen=>{
+            imagen.addEventListener('click',e=>{
+                lightbox.classList.add('active');
+                const img=document.createElement('img');
+                img.src=imagen.src;
+                if(imagen.classList.contains('vertical')){
+                    img.style.width="30%";
+                    img.style.height="100%";
+                }
+                while(lightbox.firstChild){
+                    lightbox.removeChild(lightbox.firstChild);
+                }
+                lightbox.appendChild(img);
+            })
+        });
+
+        lightbox.addEventListener('click',e=>{ // Evento de click fuera del contenedor de lightbox
+            if(e.target!==e.currentTarget) return; // e.target será básicamente el fondo negro, y e.currentTarget es el contenedor de la imagen
+            lightbox.classList.remove('active'); // Se quita la clase de active, dando un display:none por default gracias a nuestro CSS
+        });
+        /* LIGHTBOX */
+
+        /* Perros tags */
+        if(redireccionar){
+            const pagindex=document.querySelector('#index');
+            const pagcomercial=document.querySelector('#pagComercial');
+            const pagresidencial=document.querySelector('#pagResidencial');
+            const pagrecorridos=document.querySelector('#pagRecorridos');
+            const pagrenders=document.querySelector('#renders');
+            const tagEspanol=document.querySelector('.lang.espanol');
+            const tagIngles=document.querySelector('.lang.ingles');
+            const proyectoComercial=document.querySelector('a#proyectocomercial p');
+            const proyectoResidencial=document.querySelector('a#proyectoresidencial p');
+            const renders=document.querySelector('a#renders p');
+            const disenoGrafico=document.querySelector('a#disenografico p');
+            const recorridosVirtuales=document.querySelector('a#recorridosvirtuales p');
+            const tedMosby=document.querySelector('p#tedmosby');
+            const descripcion=document.querySelector('p#texto');
+            const residential=document.querySelector('h2#tial');
+            tagEspanol.addEventListener('click',e=>{
+                e.preventDefault();
+                alert("Diste click en spanish.");
+                if(pagindex){
+                    proyectoComercial.innerText="Desarrollo de estrategias de función y estética";
+                    proyectoResidencial.innerText="Proyección de comfortabilidad acorde al usuario";
+                    recorridosVirtuales.innerText="Representación cinematográfica de ideas";
+                    renders.innerText="Colaboración con diversos profesionales del diseño";
+                    disenoGrafico.innerText="Conceptualización de identidad y desarrollo de ideas";
+                    tedMosby.innerText="Arquitecto";
+                    descripcion.innerText="Apasionado por el diseño en todas sus variantes; experiencia en Interiorismo, diseño arquitectónico y diseño industrial; supervisión de obra. Aficionado del diseño gráfico y técnicas de diseño digital.";                
+                }
+                if(pagresidencial){
+                    residential.innerText="CIAL.";
+                }
+
+
+            });
+            tagIngles.addEventListener('click',e=>{
+                e.preventDefault();
+                alert("Diste click en inglish.");
+                if(pagindex){
+                    proyectoComercial.innerText="Khe1";
+                    proyectoResidencial.innerText="Khe2";
+                    renders.innerText="Khe3";
+                    disenoGrafico.innerText="Khe4";
+                    recorridosVirtuales.innerText="Khe5";
+                    tedMosby.innerText="Architect";
+                    descripcion.innerText="Jeje";                
+                }
+                if(pagresidencial){
+                    residential.innerText="TIAL."
+                }
+            });
+        }
+
         /* Fany me dijo "Te quiero mucho" y fue lo primero que leí en el día y me hizo muy feliz */
     }); // DOM CONTENT LOADED
 })();
